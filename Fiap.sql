e3461b4707172e17dfb63cbcac2d22647373f8fc
Keven Ike <kevenikepsilva@gmail.com>
2024-05-27 22:49:52 -0300

    criando as tabelas contato endereco e ong

---
+++ Fiap.sql
@@ -0,0 +1,30 @@
+CREATE TABLE T_GS_CONTATO (
+    idContato VARCHAR2(50) PRIMARY KEY,
+    nome VARCHAR2(100),
+    email VARCHAR2(100),
+    telefone VARCHAR2(20),
+    idEndereco VARCHAR2(50),
+    idOng VARCHAR2(50),
+    FOREIGN KEY (idEndereco) REFERENCES Endereco(idEndereco),
+    FOREIGN KEY (idOng) REFERENCES Ong(idOng)
+);
+
+
+CREATE TABLE T_GS_ENDERECO (
+    idEndereco VARCHAR2(50) PRIMARY KEY,
+    cep VARCHAR2(10),
+    lougradouro VARCHAR2(100)
+);
+
+CREATE TABLE T_GS_ONG (
+    idOng VARCHAR2(50) PRIMARY KEY,
+    nome VARCHAR2(100),
+    areaAtuacao VARCHAR2(100)
+);
+
+
+DROP TABLE T_GS_CONTATO;
+DROP TABLE T_GS_ENDERECO ;
+DROP TABLE T_GS_ONG;
+
+SELECT * FROM T_GS_CONTATO;
