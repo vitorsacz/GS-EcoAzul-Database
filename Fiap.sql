1afe85e5aacfb50c07497bb86da917c90fd8fc09
Vitor Santos <vitorsacz@gmail.com>
2024-06-05 16:54:01 -0300

    Alteracao os scripts de create table, sequences e inserts

--- Fiap.sql
+++ Fiap.sql
@@ -1,13 +1,8 @@
 -- Tabela de Contato
 CREATE TABLE T_GS_CONTATO (
     idContato VARCHAR2(50) PRIMARY KEY,
-    nome VARCHAR2(100) NOT NULL,
     email VARCHAR2(100) NOT NULL,
-    telefone VARCHAR2(20) NOT NULL,
-    idEndereco VARCHAR2(50),
-    idOng VARCHAR2(50),
-    FOREIGN KEY (idEndereco) REFERENCES T_GS_ENDERECO(idEndereco),
-    FOREIGN KEY (idOng) REFERENCES T_GS_ONG(idOng)
+    telefone VARCHAR2(20) NOT NULL
 );
 
 -- Tabela de Endereço
@@ -17,8 +12,7 @@ CREATE TABLE T_GS_ENDERECO (
     lougradouro VARCHAR2(100) NOT NULL,
     complemento VARCHAR2(50),
     bairro VARCHAR2(50) NOT NULL,
-    localidade VARCHAR2(50) NOT NULL,
-    uf VARCHAR2(2) NOT NULL
+    estado VARCHAR2(50) NOT NULL
 );
 
 -- Tabela de ONG
@@ -27,17 +21,84 @@ CREATE TABLE T_GS_ONG (
     nome VARCHAR2(100) NOT NULL,
     pais VARCHAR2(50) NOT NULL,
     estado VARCHAR2(50) NOT NULL,
-    areaAtuacao VARCHAR2(200) NOT NULL
+    areaAtuacao VARCHAR2(200) NOT NULL,
+    materialColetado INT NOT NULL,
+    imagem VARCHAR2(50) NOT NULL
 );
+
+
+-- TABELA AUXILIAR DE CONTATO DAS ONGS
+
+CREATE TABLE T_GS_CONTATO_ONG (
+    idContatoOng INT PRIMARY KEY,
+    idOng VARCHAR2(50),
+    idContato VARCHAR2(50),
+    FOREIGN KEY (idOng) REFERENCES T_GS_ONG(idOng),
+    FOREIGN KEY (idContato) REFERENCES T_GS_CONTATO(idContato)
+);
+
+
+-- TABELA AUXILIAR DE ENDERECO DAS ONGS
+CREATE TABLE T_GS_ENDERECO_ONG (
+    idEnderecoOng INT PRIMARY KEY,
+    idOng VARCHAR2(50),
+    idEndereco VARCHAR2(50),
+    FOREIGN KEY (idOng) REFERENCES T_GS_ONG(idOng),
+    FOREIGN KEY (idEndereco) REFERENCES T_GS_ENDERECO(idEndereco)
+);
+
+
+-- CRIANDO AS SEQUENCES
+
+-- Sequência para Tabela de Contato
+CREATE SEQUENCE SEQ_T_GS_CONTATO
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+-- Sequência para Tabela de Endereço
+CREATE SEQUENCE SEQ_T_GS_ENDERECO
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+-- Sequência para Tabela de ONG
+CREATE SEQUENCE SEQ_T_GS_ONG
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+-- Sequência para Tabela Auxiliar de Contato das ONGs
+CREATE SEQUENCE SEQ_T_GS_CONTATO_ONG
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+-- Sequência para Tabela Auxiliar de Endereço das ONGs
+CREATE SEQUENCE SEQ_T_GS_ENDERECO_ONG
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+
+
+
+
 
 -- Tabela de Praias
 CREATE TABLE T_GS_PRAIAS (
     Id_praia VARCHAR2(50) PRIMARY KEY,
     nome VARCHAR2(100) NOT NULL,
     pais VARCHAR2(50) NOT NULL,
-    estado VARCHAR2(50) NOT NULL,
     st_banho VARCHAR2(50) NOT NULL
 );
+
+
 
 -- Tabela de Plástico Produzido
 CREATE TABLE T_GS_Plastico_Produzido (
@@ -56,81 +117,66 @@ CREATE TABLE T_GS_QUALIDADE_AR_AGUA_CIDADE (
     poluicao_agua VARCHAR2(50) NOT NULL
 );
 
--- Criar a sequência para idOng
-CREATE SEQUENCE seq_idOng
-START WITH 1
-INCREMENT BY 1
-NOCACHE;
 
--- Criar o gatilho para idOng
-CREATE OR REPLACE TRIGGER trg_idOng
-BEFORE INSERT ON T_GS_ONG
-FOR EACH ROW
-BEGIN
-    :NEW.idOng := 'ONG' || TO_CHAR(seq_idOng.NEXTVAL, 'FM00000');
-END;
 
 -- Inserir dados na tabela T_GS_ONG
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Projeto Tamar', 'Brasil', 'Bahia', 'Conservação das tartarugas marinhas');
 
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Baleia Jubarte', 'Brasil', 'Espírito Santo', 'Conservação das baleias jubarte e educação ambiental');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto de Pesquisa e Conservação de Golfinhos', 'Brasil', 'Pernambuco', 'Conservação de golfinhos e pesquisa científica');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Araguaia', 'Brasil', 'Tocantins', 'Conservação da biodiversidade aquática e educação ambiental');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Associação MarBrasil', 'Brasil', 'Paraná', 'Conservação marinha, proteção de áreas costeiras e educação ambiental');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Mero s do Brasil', 'Brasil', 'Bahia', 'Conservação das tartarugas marinhas');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Meros do Brasil', 'Brasil', 'Rio de Janeiro', 'Conservação dos meros (peixes grandes), pesquisa científica e educação ambiental');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Recifes Costeiros (IRCOS)', 'Brasil', 'Pernambuco', 'Conservação de recifes costeiros e biodiversidade marinha');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Biota de Conservação', 'Brasil', 'Alagoas', 'Conservação marinha, resgate de fauna marinha e pesquisa científica');
-
-INSERT INTO T_GS_ONG (nome, pais, estado, areaAtuacao)
-VALUES ('Instituto Argonauta para Conservação Costeira e Marinha', 'Brasil', 'São Paulo', 'Conservação marinha, resgate de animais marinhos e educação ambiental');
-
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('1', 'Ocean Conservancy', 'Estados Unidos', 'Washington', 'Conservação dos Oceanos', 100, 'oceano_conservancy.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('2', 'Surfrider Foundation', 'Estados Unidos', 'California', 'Preservação das Praias', 80, 'surfrider_foundation.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('3', 'The Ocean Cleanup', 'Países Baixos', 'Holanda do Sul', 'Limpeza dos Oceanos', 150, 'ocean_cleanup.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('4', '5 Gyres', 'Estados Unidos', 'California', 'Redução da Poluição Plástica', 70, '5_gyres.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('5', 'Plastic Pollution Coalition', 'Estados Unidos', 'California', 'Combate à Poluição Plástica', 90, 'plastic_pollution_coalition.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('6', 'Instituto Ecosurf', 'Brasil', 'Rio de Janeiro', 'Preservação das Praias', 50, 'ecosurf.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('7', 'Instituto Mar Adentro', 'Brasil', 'Rio de Janeiro', 'Proteção Ambiental Marinha', 40, 'mar_adentro.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('8', 'Instituto Oceanográfico da USP', 'Brasil', 'São Paulo', 'Pesquisa Oceanográfica', 60, 'iousp.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('9', 'Projeto Tamar', 'Brasil', 'Bahia', 'Conservação de Tartarugas Marinhas', 70, 'tamar.png');
+INSERT INTO T_GS_ONG (idOng, nome, pais, estado, areaAtuacao, materialColetado, imagem)VALUES ('10', 'Instituto Coral Vivo', 'Brasil', 'Bahia', 'Conservação de Recifes de Coral', 55, 'coral_vivo.png');
 
 
 
 -- Inserir dados na tabela T_GS_ENDERECO
-INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, localidade, uf) VALUES ('END001', '12345678', 'Rua A', 'Apt 101', 'Centro', 'Cidade A', 'SP');
-INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, localidade, uf) VALUES ('END002', '23456789', 'Rua B', 'Apt 202', 'Zona Sul', 'Cidade B', 'RJ');
-INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, localidade, uf) VALUES ('END003', '34567890', 'Rua C', '', 'Zona Norte', 'Cidade C', 'MG');
-INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, localidade, uf) VALUES ('END004', '45678901', 'Rua D', 'Apt 303', 'Zona Leste', 'Cidade D', 'RS');
-INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, localidade, uf) VALUES ('END005', '56789012', 'Rua E', 'Casa 1', 'Zona Oeste', 'Cidade E', 'BA');
+
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('1', '20001-2070', '1300 19th Street NW', 'Casa', 'Dupont Circle', 'Washington');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('2', '92024', '942 S. Coast Highway 101', 'Casa', 'Encinitas', 'California');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('3', '3011 ND', 'Conradstraat 38', 'Casa', 'Rotterdam', 'Holanda do Sul');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('4', '90404', '1314 7th Street', 'Suite 290', 'Santa Monica', 'California');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('5', '90401', '502 Colorado Avenue', 'Suite 303', 'Santa Monica', 'California');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('6', '22020-040', 'Rua Dr. Rubens Gomes Bueno, 6 - Urca', 'Casa', 'Rio de Janeiro', 'Rio de Janeiro');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('7', '22410-005', 'Av. Borges de Medeiros, 1426 - Lagoa', 'Casa', 'Rio de Janeiro', 'Rio de Janeiro');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('8', '05508-120', 'Praça do Oceanográfico, 191', 'Casa', 'Butantã', 'São Paulo');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('9', '45931-000', 'Praça Dr. Odilon Junqueira, 259', 'Casa', 'Praia do Forte', 'Bahia');
+INSERT INTO T_GS_ENDERECO (idEndereco, cep, lougradouro, complemento, bairro, estado)VALUES ('10', '45931-000', 'Av. Antônio Tupy Pinheiro', 'S/N', 'Praia do Forte', 'Bahia');
+
+
+-- inserir dados na tabela T_GS_CONTATO
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('1', 'info@oceanconservancy.org', '+1 (800) 519-1541');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('2', 'contact@surfrider.org', '+1 (800) 743-SURF');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('3', 'info@theoceancleanup.com', '+31 85 888 4888');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('4', 'info@5gyres.org', '+1 (323) 786-2840');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('5', 'info@plasticpollutioncoalition.org', '+1 (415) 538-0150');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('6', 'contato@ecosurf.com.br', '(21) 98765-4321');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('7', 'contato@maradentro.org.br', '(21) 98765-4321');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('8', 'contato@iousp.br', '(11) 98765-4321');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('9', 'contato@tamar.org.br', '(71) 98765-4321');
+INSERT INTO T_GS_CONTATO (idContato, email, telefone)VALUES ('10', 'contato@coralvivo.org.br', '(71) 98765-4321');
+
+
 
 -- Inserir dados na tabela T_GS_PRAIAS
-INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, estado, st_banho) VALUES ('PRAIA001', 'Praia do Forte', 'Brasil', 'Bahia', 'Excelente');
-INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, estado, st_banho) VALUES ('PRAIA002', 'Praia de Copacabana', 'Brasil', 'Rio de Janeiro', 'Bom');
-INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, estado, st_banho) VALUES ('PRAIA003', 'Praia de Ipanema', 'Brasil', 'Rio de Janeiro', 'Regular');
-INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, estado, st_banho) VALUES ('PRAIA004', 'Praia da Pipa', 'Brasil', 'Rio Grande do Norte', 'Excelente');
-INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, estado, st_banho) VALUES ('PRAIA005', 'Praia de Jericoacoara', 'Brasil', 'Ceará', 'Boa');
+-- Praias Brasileiras
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('1', 'Praia do Diabo', 'Brasil', 'Poucas Áreas de Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('2', 'Praia da Macumba', 'Brasil', 'Poucas Áreas de Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('3', 'Praia do Leme', 'Brasil', 'Poucas Áreas de Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('4', 'Praia de Ipanema', 'Brasil', 'Poucas Áreas de Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('5', 'Praia de Copacabana', 'Brasil', 'Imprópria para Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('6', 'Venice Beach', 'Estados Unidos', 'Poucas Áreas de Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('7', 'Santa Monica Beach', 'Estados Unidos', 'Imprópria para Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('8', 'Waikiki Beach', 'Estados Unidos', 'Poucas Áreas de Banho');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('9', 'Bondi Beach', 'Austrália', 'Boa');
+INSERT INTO T_GS_PRAIAS (Id_praia, nome, pais, st_banho)VALUES ('10', 'Manly Beach', 'Austrália', 'Boa');
 
--- Inserir dados na tabela T_GS_Plastico_Produzido
-INSERT INTO T_GS_Plastico_Produzido (id_ano, ano, producao_anual) VALUES ('ANO2020', 2020, 5000000);
-INSERT INTO T_GS_Plastico_Produzido (id_ano, ano, producao_anual) VALUES ('ANO2021', 2021, 5500000);
-INSERT INTO T_GS_Plastico_Produzido (id_ano, ano, producao_anual) VALUES ('ANO2022', 2022, 6000000);
-INSERT INTO T_GS_Plastico_Produzido (id_ano, ano, producao_anual) VALUES ('ANO2023', 2023, 6500000);
-INSERT INTO T_GS_Plastico_Produzido (id_ano, ano, producao_anual) VALUES ('ANO2024', 2024, 7000000);
 
--- Inserir dados na tabela T_GS_QUALIDADE_AR_AGUA_CIDADE
-INSERT INTO T_GS_QUALIDADE_AR_AGUA_CIDADE (id_cidade, cidade, regiao, entidade, qualidade_do_ar, poluicao_agua) VALUES ('CID001', 'São Paulo', 'Sudeste', 'CETESB', 'Boa', 'Moderada');
-INSERT INTO T_GS_QUALIDADE_AR_AGUA_CIDADE (id_cidade, cidade, regiao, entidade, qualidade_do_ar, poluicao_agua) VALUES ('CID002', 'Rio de Janeiro', 'Sudeste', 'INEA', 'Regular', 'Alta');
-INSERT INTO T_GS_QUALIDADE_AR_AGUA_CIDADE (id_cidade, cidade, regiao, entidade, qualidade_do_ar, poluicao_agua) VALUES ('CID003', 'Salvador', 'Nordeste', 'INEMA', 'Boa', 'Baixa');
-INSERT INTO T_GS_QUALIDADE_AR_AGUA_CIDADE (id_cidade, cidade, regiao, entidade, qualidade_do_ar, poluicao_agua) VALUES ('CID004', 'Porto Alegre', 'Sul', 'FEPAM', 'Ruim', 'Alta');
-INSERT INTO T_GS_QUALIDADE_AR_AGUA_CIDADE (id_cidade, cidade, regiao, entidade, qualidade_do_ar, poluicao_agua) VALUES ('CID005', 'Curitiba', 'Sul', 'IAP', 'Boa', 'Baixa');
+
 
 -- Select da tabela T_GS_CONTATO
 SELECT * FROM T_GS_CONTATO;
@@ -151,6 +197,26 @@ SELECT * FROM T_GS_Plastico_Produzido;
 SELECT * FROM T_GS_QUALIDADE_AR_AGUA_CIDADE;
 
 DELETE FROM T_GS_ONG ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- Drop Table de Contato
 DROP TABLE T_GS_CONTATO;
